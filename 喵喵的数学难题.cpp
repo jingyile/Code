394693c23bca25daeac9fe47cf653b2a35837bd3
景怡乐 <39609125+jingyile@users.noreply.github.com>
2018-09-14 21:35:57 +0800

    Update 喵喵的数学难题.cpp

--- 喵喵的数学难题.cpp
+++ 喵喵的数学难题.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-#define maxx 10000
+#define maxx 100010
 int a[maxx];
 int fx(int n)
 {
 	for(int i=2;i*i<=n;i++)
 	{
-		if(n%i==0)
+		while(n%i==0)
 		{
 			a[i]++;
 			n=n/i;
@@ -29,29 +29,3 @@ int main()
 	}
 	return 0;
 }
-//int k=0;
-//int prime[maxx];
-//int vis[maxx];
-//long long fx(int n)
-//{
-//	if(n==1)
-//	return 1;
-//	return fx(n-1)*n;
-//}
-//void isprime()
-//{
-//	memset(vis,1,sizeof(vis));
-//	vis[0]=vis[1]=0;
-//	for(int i=2;i<=maxx;i++)
-//	{
-//		if(vis[i])
-//		{
-//			prime[k++]=i;
-//			for(int j=i;j*i<maxx;j++)
-//			{
-//				vis[i*j]=0;
-//			}
-//		}
-//	}
-//}
-
