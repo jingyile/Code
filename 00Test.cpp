2cee53fd97d4beb11ecb7e3fc8ecc48b1707d128
景怡乐 <39609125+jingyile@users.noreply.github.com>
2018-11-10 15:15:49 +0800

    Add files via upload

---
+++ 00Test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char a[]="1111111111111111111";
+	char b[]="111111111111111";
+char c[]="123456790123456666654320987654321";
+cout<<strlen(a)<<endl;
+cout<<strlen(b)<<endl;
+cout<<strlen(c)<<endl;
+} 
