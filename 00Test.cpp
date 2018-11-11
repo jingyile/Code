ca953325a7049a0aa50e03c573fb548736b32cc4
景怡乐 <39609125+jingyile@users.noreply.github.com>
2018-11-11 13:28:06 +0800

    Add files via upload

--- 00Test.cpp
+++ 00Test.cpp
@@ -1,11 +1,84 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<cstring>
 using namespace std;
+char mmap[105][105];
+int dir[8][2] = {{-1,0},{1,0},{0,-1},{0,1},{-1,-1},{-1,1},{1,-1},{1,1}};
+typedef struct Node
+{
+    int x;
+    int y;
+    int step;
+}node;
+int ans;
+node start,now,next;
+queue<node>q;
+void check()
+{
+    if(mmap[next.x][next.y] == '.')
+    {
+        q.push(next);
+        mmap[next.x][next.y] = '*';
+    }
+    if(mmap[next.x][next.y] == 'x')
+    {
+        q.push(next);
+    }
+}
+void bfs()
+{
+    q.push(start);
+    while(!q.empty())
+    {
+        now = q.front();
+        if(mmap[now.x][now.y] == 'x')
+        {
+            ans = now.step;
+            return;
+        }
+        for(int i = 0;i<8;i++)
+        {
+            next.x = now.x+dir[i][0];
+            next.y = now.y+dir[i][1];
+            if(dir[i][0] && dir[i][1])
+                next.step = now.step+141;
+            else
+                next.step = now.step+100;
+            check();
+        }
+        q.pop();
+    }
+    ans = -1;
+}
 int main()
 {
-	char a[]="1111111111111111111";
-	char b[]="111111111111111";
-char c[]="123456790123456666654320987654321";
-cout<<strlen(a)<<endl;
-cout<<strlen(b)<<endl;
-cout<<strlen(c)<<endl;
-} 
+    int t;
+    scanf("%d",&t);
+    int n,m;
+    while(t--)
+    {
+        memset(mmap,0,sizeof(0));
+        while(!q.empty())
+        {
+            q.pop();
+        }
+        scanf("%d %d",&n,&m);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>mmap[i][j];
+                if(mmap[i][j] == 'o')
+                {
+                    start.x = i;
+                    start.y = j;
+                    start.step = 0;
+                }
+            }
+        }
+        bfs();
+        printf("%d\n",ans);
+    }
+    return 0;
+}
